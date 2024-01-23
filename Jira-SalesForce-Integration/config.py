acb9a5a1fba3d37e22717c713e3d6b67833fe897
Naama Bendalak <74866605+8naama@users.noreply.github.com>
2024-01-23 09:54:10 +0200

    Delete Jira-SalesForce-Integration directory

--- Jira-SalesForce-Integration/config.py
+++
@@ -1,7 +0,0 @@
-# JQL query to search issues 
-QUERY = "labels = CustomerSuccessFollowUp"
-
-# API tokens
-ZENDESK_API_TOKEN = "<<ZENDESK_API_TOKEN>>"
-INTERCOM_API_TOKEN = "<<INTERCOM_API_TOKEN>>"
-ATLASSIAN_API_TOKEN = "<<ATLASSIAN_API_TOKEN>>"
